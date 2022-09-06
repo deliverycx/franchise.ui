6add6973271d292f9d49ec8a9ebb8cd609347c0f
git4res <akuzin20@gmail.com>
2022-09-06 15:13:07 +0300

    main info video 2

--- src/components/MainInfo.tsx
+++ src/components/MainInfo.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import Link from "next/link";
 
 export function MainInfo() {
+
     return (
-        <main className="main container">
-            <div className="main__text-primary">Самая крупная <br/>
-                <span className="main__text-primary--highlight">сетей хинкальных </span>
-                <br/>в России
-            </div>
-            <div className="main__text-secondary">
-                Станьте владельцем уютной хинкальной <br/>по франшизе с прибылю 4.5 млн рублей в год
-            </div>
-            <a href="#" className="main-btn">Получить бизнес-план</a>
-            <div className="main__rewards"></div>
-            <div className="main__video">
-                <div
-                    className="col-6 characteristic_h1  section-btn d-flex justify-content-md-end align-self-center">
-                    <Link
-                        href="https://www.youtube.com/channel/fmsiLPKVYVY">
-                        <a className="btn" target="_blank"> Смотреть всё</a>
-                    </Link>
+        <main className="main">
+            <div className="container">
+                <div className="main__text-primary">Самая крупная <br/>
+                    <span className="main__text-primary--highlight">сетей хинкальных </span>
+                    <br/>в России
                 </div>
+                <div className="main__text-secondary">
+                    Станьте владельцем уютной хинкальной <br/>по франшизе с прибылю 4.5 млн рублей в год
+                </div>
+                <a href="#" className="main-btn">Получить бизнес-план</a>
+                <div className="main__rewards"></div>
+            </div>
+            <div className="main__video">
+                <iframe
+                    id="existing-iframe-example"
+                    width="100%" height="900"
+                    src="https://www.youtube.com/embed/fmsiLPKVYVY?autoplay=1&mute=1&enablejsapi=1"
+                    frameBorder="0"
+                ></iframe>
             </div>
         </main>
     );
