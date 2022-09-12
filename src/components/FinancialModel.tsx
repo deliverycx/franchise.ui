fefa3083766d651ba9e5abbf97f17e54726cc65b
git4res <akuzin20@gmail.com>
2022-09-12 15:05:10 +0300

    financial model

--- src/components/FinancialModel.tsx
+++ src/components/FinancialModel.tsx
@@ -2,14 +2,51 @@ import React from 'react';
 
 export function FinancialModel() {
 
-    // const infoBlocks = [
-    //     {
-    //         id: 1,
-    //         iconUrl:,
-    //         primaryTxt:,
-    //         secondaryTxt:
-    //     }
-    // ]
+    const leftInfoBlocks = [
+        {
+            id: 1,
+            iconUrl: "/assets/icons/money-blue-circle.svg",
+            title: '2 700 000 ₽',
+            sub: 'Выручка в месяц'
+        },
+        {
+            id: 2,
+            iconUrl: "/assets/icons/paper-grey-circle.svg",
+            title: '90 000 ₽',
+            sub: 'Выручка в день'
+        },
+        {
+            id: 3,
+            iconUrl: "/assets/icons/paperL-grey-circle.svg",
+            title: '100',
+            title_sub: '/ день',
+            sub: 'Количество чеков'
+        },
+    ]
+
+    const rightInfoBlocks = [
+        {
+            id: 1,
+            iconUrl: "/assets/icons/lamp-grey-circle.svg",
+            title: '16,8%',
+            sub: 'Прибыль в месяц'
+        },
+
+        {
+            id: 2,
+            iconUrl: "/assets/icons/money-grey-circle.svg",
+            title: '900 ₽',
+            sub: 'Средний чек'
+        },
+
+        {
+            id: 3,
+            iconUrl: "/assets/icons/paperL-grey-circle.svg",
+            title: '3 000',
+            title_sub: '/ месяц',
+            sub: 'Количество чеков'
+        },
+    ]
 
     return (
         <section className="financial-model container">
@@ -19,12 +56,42 @@ export function FinancialModel() {
             </div>
             <div className="financial-model__blocks">
                 <div className="financial-model__info-blocks">
-                    <div className="info-block">
-                        <img src="/assets/icons/contribution.svg" className="icon" alt="icon"/>
+                    <div className="financial-model__info-blocks--left">
+                        {
+                            leftInfoBlocks.map(el => {
+                                return <div className="info-block" key={el.id}>
+                                    <img src={el.iconUrl} className="icon" alt="icon"/>
+                                    <div className="info">
+                                        <div className="title">{el.title} <sup className="title-sub">{el.title_sub}</sup></div>
+                                        <div className="sub">{el.sub}</div>
+                                    </div>
+                                </div>
+                            })
+                        }
+                    </div>
+                    <div className="financial-model__info-blocks--right">
+                        {
+                            rightInfoBlocks.map(el => {
+                                return <div className="info-block" key={el.id}>
+                                    <img src={el.iconUrl} className="icon" alt="icon"/>
+                                    <div className="info">
+                                        <div className="title">{el.title} <sup className="title-sub">{el.title_sub}</sup></div>
+                                        <div className="sub">{el.sub}</div>
+                                    </div>
+                                </div>
+                            })
+                        }
                     </div>
                 </div>
-                <div className="financial-model__moving-block">
-
+                <div className="financial-model__right-block">
+                    <div className="top-text">Выберите <br/>
+                        <b >лучшее решение </b><br/>
+                        для ваших инвестиций</div>
+                    <div className="bottom-block">
+                        <div className="income">454 000 ₽</div>
+                        <div className="sub">Прибыль в месяц</div>
+                        <a href="#" className="main-btn">Получить бизнес-план</a>
+                    </div>
                 </div>
             </div>
         </section>
